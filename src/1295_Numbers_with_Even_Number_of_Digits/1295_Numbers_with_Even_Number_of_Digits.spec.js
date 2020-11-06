bf32b8ab20c7ae0e7554ccff5155b1557873a5dc
ababenko <alina.babenko@482.solutions>
2020-11-06 17:12:02 +0200

    1295. Find Numbers with Even Number of Digits

---
+++ src/1295_Numbers_with_Even_Number_of_Digits/1295_Numbers_with_Even_Number_of_Digits.spec.js
@@ -0,0 +1,30 @@
+import fib from './509_Fibonacci_Number';
+const { assert } = require('chai');
+
+describe('Fibonacci Number', () => {
+  const testCase = [
+    {n: 1, expected: 1 },
+    {n: 2, expected: 1 },
+    {n: 3, expected: 2 },
+    {n: 4, expected: 3 },
+    {n: 5, expected: 5 },
+    {n: 6, expected: 8 },
+    {n: 7, expected: 13 },
+    {n: 8, expected: 21 },
+    {n: 9, expected: 34 },
+    {n: 10, expected: 55 },
+    {n: 15, expected: 610 },
+    {n: 20, expected: 6765 },
+    {n: 30, expected: 832040 },
+    {n: 50, expected: 12586269025 },
+    {n: 90, expected: 2880067194370816000 },
+    {n: 110, expected: 4.356677625885486e+22 },
+    {n: 200, expected: 2.8057117299251016e+41 },
+  ];
+
+  testCase.forEach( (testCase) => {
+    it(`Fiboncci number from ${testCase.n} to equal ${testCase.expected}`, () => {
+      assert.equal(fib(testCase.n), testCase.expected);
+    });
+  });
+});
